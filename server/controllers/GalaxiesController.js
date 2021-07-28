5ab2963e4988c3160df0b38b6c0d90e6f9dc1c12
Enayatullal Shadab <Enayatullah_Shadab@yahoo.com>
2021-07-28 15:44:51 -0600

    init

---
+++ server/controllers/GalaxiesController.js
@@ -0,0 +1,62 @@
+import { query } from 'express'
+import { galaxiesService } from '../services/GalaxiesService'
+
+export class GalaxiesController extends BaseController {
+  constructor() {
+    super('api/galaxies')
+    this.router
+
+      .get('', this.getAll)
+      .get('/:id', this.getById)
+      .post('', this.create)
+      .put('/:id', this.edit)
+      .delete('/:id', this.destroy)
+    this.router = undefined
+  }
+
+  async getAll(req, res, next) {
+    try {
+      const galaxies = await galaxiesService.getAll(req, query)
+      res.send(galaxies)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async getById(req, res, next) {
+    try {
+      const galaxy = await galaxiesService.getById(req.params.id)
+      res.send(galaxy)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async create(req, res, next) {
+    try {
+      const galaxy = await galaxiesService.create(req.body)
+      res.send(galaxy)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async edit(req, res, next) {
+    try {
+      req.body.id = req.params.id
+      const galaxy = await galaxiesService.edit(req.body)
+      res.send(galaxy)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async destroy(req, res, next) {
+    try {
+      const galaxy = await galaxiesService.destroy(req.params.id)
+      res.send(galaxy)
+    } catch (error) {
+      next(error)
+    }
+  }
+}
--- server/db/DbContext.js
+++ server/db/DbContext.js
@@ -1,8 +1,9 @@
-import ValueSchema from '../models/Value'
 import mongoose from 'mongoose'
+import GalaxySchema from '../models/Galaxy'
 
 class DbContext {
-  Values = mongoose.model('Value', ValueSchema);
+  // Values = mongoose.model('Value', ValueSchema);
+  Galaxies = mongoose.model('Galaxy', GalaxySchema);
 }
 
 export const dbContext = new DbContext()
